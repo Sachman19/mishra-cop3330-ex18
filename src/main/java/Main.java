67bce48b900c98f6d366dc002a49a84e37bbb3d1
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 07:24:25 -0400

    Version 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Press 1 if you wish to convert from Fahrenheit to Celsius, Press 0 to convert from Celsius to Fahrenheit");
+        int temp = input.nextInt();
+        double Fah = 0, Cel = 0;
 
+        System.out.print("Please enter the temperature in ");
+        if(temp == 1){
+            System.out.print("Fahrenheit. ");
+            Fah = input.nextDouble();
+            Cel = (Fah - 32) * 5 / 9;
+        }
+        else if(temp == 0){
+            System.out.print("Celsius. ");
+            Cel = input.nextDouble();
+            Fah = (Cel * 9 / 5) + 32;
+        }
+        else{
+            System.exit(-1);
+        }
 
+        System.out.print("The temperature in ");
+
+        if(temp == 1){
+            System.out.printf("Celsius is %.2f. ", Cel);
+        }
+        else {
+            System.out.printf("Fahrenheit is %.2f. ", Fah);
+        }
 
     }
 }
